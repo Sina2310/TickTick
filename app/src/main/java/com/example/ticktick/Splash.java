374fd21a13f1bad440de8d89ffe3a95b969ebe68
Sina Patel <sinapatel05@gmail.com>
2020-06-18 13:58:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ticktick/Splash.java
@@ -0,0 +1,34 @@
+package com.example.ticktick;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+
+// Using handler with postDelayed called runnable run method
+
+            @Override
+
+            public void run() {
+
+                Intent i = new Intent(Splash.this, Login.class);
+
+                startActivity(i);
+
+                // close this activity
+
+                finish();
+
+            }
+
+        }, 5*1000);
+    }
+}
